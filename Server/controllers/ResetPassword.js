0434ccccfe67cacd84a368dc090b94b6e5c6ec48
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2024-03-05 21:57:09 +0530

    reset password code updated

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -32,7 +32,10 @@ exports.resetPasswordToken = async (req, res) => {
     );
 
     // Create Url
-    const url = `http://localhost:3000/update-password/${token}`;
+    // const url = `http://localhost:3000/update-password/${token}`;
+    
+    const url = `http://lstudynotion-jgok.onrender.com/api/v1/update-password/${token}`
+
 
     // Send mail containing the url
     await mailSender(email, "Password reset link", `Link: ${url}`);
